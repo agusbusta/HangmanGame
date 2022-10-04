e48fe20036034bf7f049e97aa719454ad9205787
agusbusta <agusbus95@gmail.com>
2022-10-03 21:12:21 -0300

    function added

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const words = 'HOLA COMO ESTAS'
+const palabras = ['manantial','agua','tierra','casa','rodilla','tortuga','hamburguesa','pan','tortuga','alura']
+
+function iniciarNuevoJuego(){
+    let palabra = palabras[Math.floor((Math.random() * palabras.length))].toUpperCase();
+    selectedWord = palabra.split('');
+    alert(palabra)
+}
+
