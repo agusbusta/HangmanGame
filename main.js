6fbbfa698d67279f599070622fccacc965cc418c
agusbusta <agusbus95@gmail.com>
2022-10-06 09:34:03 -0300

    go to added

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const palabras = ['manantial','agua','tierra','casa','rodilla','tortuga','hambur
 
 function iniciarNuevoJuego(){
     let palabra = palabras[Math.floor((Math.random() * palabras.length))].toUpperCase();
-    palabra = palabra.split('');
     document.getElementById("buttonIniciarJuego").style.display = "none";
     document.getElementById("buttonAgregarNuevaPalabra").style.display = "none";
+    sessionStorage.setItem("palabraElegida",palabra);
+    window.location.href="game.html";
 }
 
 //funcion para agregar nueva palabra al array de palabras.
@@ -17,25 +18,12 @@ function agregarNuevaPalabra(){
     window.location.href="game.html";
 }
 
-// funcion para reiniciar juego en pagina Game - boton nuevo juego. 
-function restartGame(){
-    window.location.reload();
-}
 
 //funcion para ir a pagina agregar palabra.
 
 function goToNuevaPalabra(){
     window.location.href="nuevaPalabra.html";
 }
-
-//funcion para ir al index.
-
-function goToIndex(){
-    window.location.href="index.html";
-}
-
-
-
 
 
 
