512cf8f6b59ac366d218cdb911e29d4294a30f76
agusbusta <agusbus95@gmail.com>
2022-10-10 18:46:08 -0300

    finished

--- game.js
+++ game.js
@@ -13,11 +13,12 @@ function goToIndex() {
 //funcion para remplazar palabra elegida random por renglones.
 
 function palabraElegida() {
+
     const palabraElegida = sessionStorage.getItem("palabraElegida");
     let cantidaDeLetras = palabraElegida.length;
     let letra = ""
     for (i = 0; i < cantidaDeLetras; i++) {
-        letra += `<span id="${i}"> _ </span>`;
+        letra += `<span id="${i}" class="guion"> _ </span>`;
     }
     document.getElementById("guiones").innerHTML = letra;
     let pantalla = document.querySelector("canvas");
@@ -28,59 +29,79 @@ function palabraElegida() {
 //Detectar tecla presionada y hacerla mayuscula. 
 
 document.addEventListener("keydown", teclaPresionada);
+let errores = 0;
 
 function teclaPresionada(event) {
     const teclaPresionada = event.key.toUpperCase()
     const palabraElegida = sessionStorage.getItem("palabraElegida");
     let letrasSeparadas = palabraElegida.split('');
-    const letraAcertada = document.getElementById("guiones").value;
+    const letraAcertada = document.getElementById("guiones");
     const letraErrada = document.getElementById("errado");
     let pantalla = document.querySelector("canvas");
     let pincel = pantalla.getContext("2d");
-    let errores = 0;
 
+    // solo se lee de la A-Z. 
+    const pattern = new RegExp ('^[A-Z]+$', 'i');
 
-    if(!palabraElegida.includes(teclaPresionada)){
-        errores++;
-        switch (errores) {
-            case 1:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarhorca(pincel);
-                break;
-            case 2:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarcabeza(pincel);
-                break;
-            case 3:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujartronco(pincel);
-                break;
-            case 4:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarbrzizq(pincel);
-                break;
-            case 5:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarbrzder(pincel);
-                break;
-            case 6:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarlegder(pincel);
-                break;
-            case 7:
-                letraErrada.innerHTML += teclaPresionada;
-                dibujarlegizq(pincel);
-                break;
+    if(pattern.test(teclaPresionada)){
+        console.log("entro");
+
+    // Condicional de errores y aciertos.
+    
+    if (!palabraElegida.includes(teclaPresionada)) {
+        if (errores < 7) {
+            errores += 1;
+            switch (errores) {
+                case 1:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarhorca(pincel);
+                    break;
+                case 2:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarcabeza(pincel);
+                    break;
+                case 3:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujartronco(pincel);
+                    break;
+                case 4:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarbrzizq(pincel);
+                    break;
+                case 5:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarbrzder(pincel);
+                    break;
+                case 6:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarlegder(pincel);
+                    break;
+                case 7:
+                    letraErrada.innerHTML += teclaPresionada;
+                    dibujarlegizq(pincel);
+                    break;
+            }
+        }
+        else {
+            document.getElementById("perdiste").style.display = "inline-block";
         }
     }
-
-    else{
+    else {
         for (contador = 0; contador < letrasSeparadas.length; contador++) {
             let spanGuion = document.getElementById(`${contador}`);
             if (teclaPresionada == letrasSeparadas[contador]) {
-            spanGuion.innerHTML = teclaPresionada;
-            }}}}
-
+                spanGuion.innerHTML = teclaPresionada;
+                spanGuion.classList.remove("guion");
+            }
+        }
+        let guion = document.getElementsByClassName("guion");
+        if (guion.length == 0) {
+        document.getElementById("ganaste").style.display = "inline-block";
+    
+        }
+    }
+}
+}
 //Secciones de dibujar horca
 
 var lienzo = document.querySelector("#dibujoahorcado");
@@ -168,7 +189,3 @@ function dibujarlegder(pincel) {
     pincel.lineTo(385, 450);
     pincel.stroke();
 }
-
-
-
-
